1e33a47156ad6c7ee2a5b336dfa8644250e72337
Daniel Oyebanjo <daniel@korapay.com>
2026-05-09 15:51:57 +0100

    style(login): increase max width of login form container
    Adjusts the login form container width from `max-w-xs` to `max-w-lg`
    to provide more horizontal space for form elements.

--- src/routes/views.login.tsx
+++ src/routes/views.login.tsx
@@ -15,19 +15,19 @@ function RouteComponent() {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <img src={logo} alt="Logo" />
             </div>
             Kora
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
+          <div className="w-full max-w-lg">
             <LoginForm />
           </div>
         </div>
       </div>
-      <div className="relative hidden bg-muted lg:block">
+      <div className="bg-muted relative hidden lg:block">
         <img
           src={placeholder}
           alt="Image"
